15ed84e9028ab4cc189d74b276a6e82d175b8b73
Bryan Flores <59964192+BryanFloresITQ@users.noreply.github.com>
2020-01-20 09:05:34 -0500

    Actualización Getters y Setters

--- Lenguaje de Programación 2/src/Modelo/Persona.java
+++ Lenguaje de Programación 2/src/Modelo/Persona.java
@@ -5,10 +5,99 @@
  */
 package Modelo;
 
+import java.util.logging.Logger;
+
 /**
  *
  * @author Bryan
  */
 public class Persona {
+    
+    String nombre;
+    String apellido;
+    int cedula;
+    int edad;
+    int telefono;
+    String direccion;
+    char genero;
+    boolean EstadoCivil;
+
+    public Persona(String nombre, String apellido, int cedula, int edad, int telefono, String direccion, char genero, boolean EstadoCivil) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.genero = genero;
+        this.EstadoCivil = EstadoCivil;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public boolean isEstadoCivil() {
+        return EstadoCivil;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public void setEstadoCivil(boolean EstadoCivil) {
+        this.EstadoCivil = EstadoCivil;
+    }
+    
+    
+    
     
 }
