b3776375d99c3e931b7fb920249e77fb012a7b14
Bryan Flores <59964192+BryanFloresITQ@users.noreply.github.com>
2020-01-27 09:08:34 -0500

    Conexion exitosa

--- Enlace a la Base de Datos/src/conexion/conector.java
+++ Enlace a la Base de Datos/src/conexion/conector.java
@@ -5,10 +5,56 @@
  */
 package conexion;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Bryan
  */
 public class conector {
+    
+    Connection conexion = null;
+    
+    String url="jdbc:mysql://localhost:3306/base?useSSL=false"; //puerto generalmente 3306 y el nombre de la base
+    String usuario="root"; 
+    String password="Listo123";
+    
+    
+    public conector(){
+    
+        try {
+            conexion=(Connection) DriverManager.getConnection(url,usuario,password);
+            if(conexion!=null){
+            
+                System.out.println("Conexión Exitosa");
+            
+            }else{
+            
+                System.out.println("No hubo conexión");
+            
+            }
+        } catch (SQLException ex) {
+            
+            System.err.println("No se pudo conectar");
+            ex.printStackTrace();
+        }
+    
+    }
+    
+    public Connection getConexion(){
+    
+        return conexion;
+    
+    }
+    
+    public void Desconectar(){
+    
+        conexion = null;
+    
+    }
     
 }
