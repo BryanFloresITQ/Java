437ca2f0eda160fdbca81122eae3e2b2afe3da9c
Bryan Flores <59964192+BryanFloresITQ@users.noreply.github.com>
2020-01-28 09:32:49 -0500

    Actualizacion

--- Enlace a la Base de Datos/src/enlaceBDD/EnlaceBDD.java
+++ Enlace a la Base de Datos/src/enlaceBDD/EnlaceBDD.java
@@ -1,52 +1,59 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package enlaceBDD;
-//
-//import Modelo.Modelo;
-//import Vista.producto;
-//import conexion.conector;
-//import java.sql.PreparedStatement;
-//import java.sql.SQLException;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//
-///**
-// *
-// * @author Bryan
-// */
-//public class EnlaceBDD {
-//
-//    
-//    
-//    
-//    public static void main(String[] args) {
-//        
-//        Modelo modelo=new Modelo();
-//        
-//        PreparedStatement ps=null;
-//        
-//        conector conn=new conector();
-//        
-//        conn.getConexion();
-//        
-//        String insert="insert into "+ "articulos(nombre,descripcion,precio) "+ "values(?,?,?) ";
-//        try {
-//            ps = conn.getConexion().prepareStatement(insert);
-//            ps.setString(1, modelo.getNombre());
-//            ps.setString(2, modelo.getDescripcion());
-//            ps.setDouble(3, modelo.getPrecio());
-//            ps.executeUpdate();
-//            
-//        } catch (SQLException ex) {
-//            Logger.getLogger(EnlaceBDD.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        
-//        
-//    }
-//    
-//    
-//    
-//}
+
+package enlaceBDD;
+
+import Modelo.Modelo;
+import Vista.producto;
+import conexion.conector;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Bryan
+ */
+public class EnlaceBDD {
+
+    
+    
+    
+    public static void main(String[] args) {
+        
+        Modelo modelo=new Modelo();
+        
+        PreparedStatement ps=null; //Ingresar Datos
+        ResultSet rs=null;     // Obtener Datos
+        
+        conector conn=new conector();
+        
+        conn.getConexion();
+        
+        String select = "select * from articulos where idArticulo = ?";
+        
+        try {
+            ps = conn.getConexion().prepareStatement(select);
+            
+             ps.setInt(1, 1);
+             rs = ps.executeQuery();
+             
+             while (rs.next()) {                
+                
+                 System.out.println(rs.getString(2));
+                 System.out.println(rs.getString(3));
+                 System.out.println(rs.getString(4));
+                 
+            }
+             
+        } catch (SQLException ex) {
+            Logger.getLogger(EnlaceBDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+       
+        
+    }
+    
+    
+    
+}
